22fc452e6deb8551e628c35f92db16ac83dfceee
petrero <petrero@gmail.com>
2017-05-22 19:53:44 +0300

    While we're here, I'll add a use statement for MinkContext and make it extend that. I'll explain that in a minute.
    One last bit of setup: at the root of your project create a behat.yml file. I'll paste in some content to get us started.
    
    When we run Behat it will looks for a behat.yml file and this tells it:
    
    Yo! Our application lives at localhost, so look for it there.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -3,11 +3,13 @@
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+use Behat\Behat\Context\SnippetAcceptingContext;
 
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext implements Context
+class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
 {
     /**
      * Initializes context.
