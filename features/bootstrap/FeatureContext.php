07e7d4138b6d2c628e07ea442e4ad1276677a230
petrero <petrero@gmail.com>
2017-05-25 19:35:48 +0300

    Copy code from branch 4

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,51 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+use Behat\Behat\Context\SnippetAcceptingContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext {
+  private $output;
+  /**
+   * Initializes context.
+   *
+   * Every scenario gets its own context instance.
+   * You can also pass arbitrary arguments to the
+   * context constructor through behat.yml.
+   */
+  public function __construct()
+  {
+  }
+  
+  /**
+   * @Given there is a file named :filename
+   */
+  public function thereIsAFileNamed($filename)
+  {
+    touch($filename);
+  }
+
+  /**
+   * @When I run :command
+   */
+  public function iRun($command)
+  {
+    $this->output = shell_exec($command);
+  }
+
+  /**
+   * @Then I should see :string in the output
+   */
+  public function iShouldSeeInTheOutput($string)
+  {
+    if (strpos($this->output, $string) === false){
+      throw new \Exception(sprintf('Did not see "%s" in output "%s"', $string, $this->output));
+    }
+  }
+    
+}
