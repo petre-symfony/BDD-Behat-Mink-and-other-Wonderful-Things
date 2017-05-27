7854401349fe96aa6036d719214bf9e7c196e902
petrero <petrero@gmail.com>
2017-05-27 23:31:46 +0300

    In ProductAdminController, set the author when a product is created: {dollar_sign}product->setAuthor({dollar_sign}this->getUser())
    Run Behat again:
    
    ./vendor/bin/behat features/web/product_admin.feature:19
    Fixed!
    
    And that folks, is behavior driven development. It's useful, and a bucket of fun. It forces you to design the behavior of your code. But it also helps you know when you're finished. If the scenario is green, stop coding and over-perfecting things. And yes, someone will need to add a designer's touch for a really nice UI, But from a behavioral perspective, this feature does what it needs to do. So move onto what's next.

--- src/AppBundle/Controller/ProductAdminController.php
+++ src/AppBundle/Controller/ProductAdminController.php
@@ -31,6 +31,7 @@ class ProductAdminController extends Controller {
       $product->setName($request->get('name'));
       $product->setDescription($request->get('description'));
       $product->setPrice($request->get('price'));
+      $product->setAuthor($this->getUser());
       
       $em = $this->getDoctrine()->getManager();
       $em->persist($product);
