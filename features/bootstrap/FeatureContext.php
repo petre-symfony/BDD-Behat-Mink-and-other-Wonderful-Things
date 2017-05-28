c13f162e3a43cf68061c5440a7919a5b84202bce
petrero <petrero@gmail.com>
2017-05-28 12:13:19 +0300

    Change the language to 'I save a screenshot to' and remove {dollar_sign}this->screenshotDir thing since we don't have that. To save screenshots to the root of your project, replace it with __DIR__'/../../'
    Run it!
    
    ./vendor/bin/behat features/web/product_admin.feature:20
    The modal opens and it still fails at the 'New Product' step. But now we have a fancy new shot.png file at the root of the project that shows exactly what things looked like when the test failed. Woah.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -177,10 +177,10 @@ class FeatureContext extends RawMinkContext implements Context, SnippetAccepting
   /**
      * Saving a screenshot
      *
-     * @When I save a screenshot in :filename
+     * @When I save a screenshot to :filename
      */
   public function iSaveAScreenshotIn($filename){
     sleep(1);
-    $this->saveScreenshot($filename, $this->screenshotDir);
+    $this->saveScreenshot($filename, __DIR__.'/../../');
   }
 }
